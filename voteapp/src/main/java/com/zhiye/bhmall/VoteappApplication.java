5124c90a73046290e839009098da66bf25fc3bdd
huangjixin <517714860@qq.com>
2019-01-15 16:52:48 +0800

    添加小结

--- voteapp/src/main/java/com/zhiye/bhmall/VoteappApplication.java
+++ voteapp/src/main/java/com/zhiye/bhmall/VoteappApplication.java
@@ -7,12 +7,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.Environment;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import com.alibaba.druid.pool.DruidDataSource;
 
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 import tk.mybatis.spring.annotation.MapperScan;
 
+@EnableTransactionManagement//开启事务管理
 @SpringBootApplication
 @EnableSwagger2
 @MapperScan({ "com.zhiye.bhmall.modules.vote.mapper" })
